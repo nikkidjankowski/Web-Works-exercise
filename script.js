213266572c963ed4b8fa8c656d345f96ed45a083
nikkidjankowski <79601782+nikkidjankowski@users.noreply.github.com>
2022-03-02 13:31:29 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+/*
+
+
+
+1.What is HTTP?
+http is a hypertext transfer protocol and helps fetch HTML documents
+
+2.What is a URL?
+it is the uniform resource locator which helps references a web source
+
+3.What is DNS?
+The naming system to identify the hierarchy of the computers and their systems
+
+
+4.What is a query string?
+its a resource locator to pass key value pairs into the url
+
+5.What are two HTTP verbs and how are they different?
+The two verbs are GET and POST. GET gets some data from the server while
+POST sends dara to the server
+
+6.What is an HTTP request?
+Its when the client makes a request to the server
+
+7.What is an HTTP response?
+The response is made by the server to the client
+
+8.What is an HTTP header? Give a couple examples of request and response
+ headers you have seen.
+The header helps give more information about the request or response. Some 
+request headers are Host and Cookies. Some response headers include content type
+and set-cookie.
+
+
+
+9.What are the processes that happen when you
+ type “http://somesite.com/some/page.html” into a browser?
+
+ The browser creates an IP address using DNS. Then the browser makes a request using the ip address
+ and looking for any request headers. Next the server will communicate if it was successful.
+ The browser then creates a DOM for that http and any requests for links in the page
+
+
+
+*/
